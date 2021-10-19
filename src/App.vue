e8f6474dfba7c44a30dd0091e57e89e50102a211
Matheus <matheuspsilva29@gmail.com>
2021-10-19 16:04:31 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="container">
+    <div class="col-12 d-flex justify-content-between my-5">
+      <h1 class=""> Todo List</h1>
+      <a href="" class="btn btn-success">Criar Tarefa</a>
+    </div>
+
+    <ul class="lista">
+      <li v-for="tarefa of tarefas" class="item">
+        <card-tarefa :title="tarefa.title" @removerTarefa="remove(tarefa)"></card-tarefa>
+      </li>
+    </ul>
+
+  </div>
+</template>
+
+<script>
+import CardTarefa from './components/shared/card-tarefa/CardTarefa.vue';
+
+export default {
+  name: 'app',
+  components:{
+    'card-tarefa': CardTarefa
+  },
+  data () {
+    return {
+      tarefas: {
+        tarefa: {
+          title: "Limpar a casa"
+        },
+        tarefa2: {
+          title: "Estudar vuejs"
+        },
+        tarefa3: {
+          title: "Fazer exercício físico"
+        },
+
+      }
+    }
+  },
+    methods:{
+
+    remove(tarefa){
+      alert("remover tarefa " + tarefa.title);
+    }
+
+  },
+}
+</script>
+
+<style>
+
+.lista{
+
+  list-style: none;
+  margin-left: 0px;
+  padding-left: 0%;
+
+}
+
+.titulo-principal{
+  text-align: center;
+}
+
+</style>
---
+++ src/components/shared/card-tarefa/CardTarefa.vue
@@ -0,0 +1,33 @@
+<template>
+
+  <div class="card">
+    <div class="card-body">
+      <div class="row">
+        <div class="col-12 d-flex justify-content-between">
+          <h3 class="">{{ title }}</h3>
+          <button class="btn btn-danger" v-on:click="removerTarefa()" type="submit">Remover</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  props: ['title'],
+
+  methods:{
+    removerTarefa(){
+      this.$emit('removerTarefa');
+    }
+  }
+}
+</script>
+
+<style scoped>
+.row{
+  width: auto;
+}
+
+</style>
