cd830a34fdb12ced9245f997e3b301eb748fcb53
Matheus <matheuspsilva29@gmail.com>
2021-10-27 16:45:52 -0300

    Add create, read and delete requests

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,29 @@
   <div class="container">
     <div class="col-12 d-flex justify-content-between my-5">
       <h1 class=""> Todo List</h1>
-      <a href="" class="btn btn-success">Criar Tarefa</a>
+          <div>
+      <b-button v-b-modal.modal-1>Adicionar Tarefa</b-button>
+
+      <b-modal id="modal-1" title="Cadastro de tarefa" hide-footer>
+        <div class="row">
+          <div class="col-12">
+            <b-form-input v-model="title" placeholder="nome"></b-form-input>
+          </div>
+        </div>
+
+        <div class="row mt-4">
+          <div class="col-12">
+            <button @click="addTarefa" class="btn btn-primary">Salvar</button>
+          </div>
+        </div>
+
+      </b-modal>
+    </div>
     </div>
 
     <ul class="lista">
       <li v-for="(tarefa, key) of tarefas" class="item" v-bind:key="tarefa.id">
-        <card-tarefa :title="tarefa.title" @removerTarefa="remove(key)"></card-tarefa>
+        <card-tarefa :title="tarefa.title" :id="tarefa.id" @removerTarefa="remove(tarefa.id)"></card-tarefa>
       </li>
     </ul>
 
@@ -24,27 +41,50 @@ export default {
   },
   data () {
     return {
-      tarefas: [
-        {
-          title: "Limpar a casa"
-        },
-        {
-          title: "Estudar vuejs"
-        },
-        {
-          title: "Fazer exercício físico"
-        },
-
-      ]
+      tarefas: [],
+      title: '',
+      id:''
     }
   },
-    methods:{
-
+  methods:{
     remove(tarefa){
-      this.tarefas.splice(tarefa, 1);
+      this.$http.delete(`tarefas/${tarefa}`)
+      .then(response => {
+					console.log(response);
+			})
+			.catch(error => {
+				console.log( error );
+			})
+    },
+    getList(){
+      this.$http.get('/tarefas')
+      .then(response => {
+					this.tarefas = response.data;
+			})
+			.catch(error => {
+				console.log( error );
+			})
+    },
+    addTarefa(){
+
+    var data = {
+      title: this.title,
     }
 
+    this.$http.post('/tarefas', data).then((response)=>{
+      console.log(response)
+    })
+
+    this.tarefas.push(data);
+
+    this.title = ''
+
+    }
   },
+  created(){
+    //Chamar função get list quando este componente for criado
+    this.getList();
+  }
 }
 </script>
 
--- src/components/shared/card-tarefa/CardTarefa.vue
+++ src/components/shared/card-tarefa/CardTarefa.vue
@@ -3,8 +3,10 @@
   <div class="card">
     <div class="card-body">
       <div class="row">
-        <div class="col-12 d-flex justify-content-between">
+        <div class="col-10">
           <h3 class="">{{ title }}</h3>
+        </div>
+        <div class="col-2">
           <button class="btn btn-danger" v-on:click="removerTarefa()" type="submit">Remover</button>
         </div>
       </div>
@@ -15,14 +17,13 @@
 
 <script>
 export default {
-  props: ['title'],
-
+  props: ['title', 'id'],
   methods:{
     removerTarefa(){
       if(confirm('Confirma operacao?')) {
         this.$emit('removerTarefa');
       }
-    }
+    },
   }
 }
 </script>
